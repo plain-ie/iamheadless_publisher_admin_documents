a1f7c43bb39c002d3a84d938a3f9771754dbf657
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-08 11:01:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# iamheadless_publisher_admin_documents
+
+App to render homepage item type in `iamheadless_publisher_admin` frontend.
+
+## Installation
+
+Requires `iamheadless_publisher_admin`
+Requires `iamheadless_publisher_file_handling`
+
+1. install package
+2. add `iamheadless_publisher_admin_documents` to `INSTALLED_APPS` in `settings.py`
+3. add `'iamheadless_publisher_admin_documents.pydantic_models.DocumentPydanticModel'` to `IAMHEADLESS_PUBLISHER_ADMIN_SERIALIZER_LIST` in `settings.py`
---
+++ iamheadless_publisher_admin_documents/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import formset_factory
+
+
+class DocumentContentForm(forms.Form):
+    language = forms.CharField(initial='', max_length=255, widget=forms.widgets.HiddenInput())
+    title = forms.CharField(initial='', max_length=255)
+    summary = forms.CharField(initial='', widget=forms.widgets.Textarea())
+    file_name = forms.CharField(initial='', max_length=4096, widget=forms.widgets.HiddenInput())
+    file = forms.FileField(required=False)
+
+
+DocumentContentFormSet = formset_factory(DocumentContentForm, extra=0)
+
+
+class DocumentForm(forms.Form):
+    pass
